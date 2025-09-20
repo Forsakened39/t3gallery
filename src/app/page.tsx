84e52084f659743c41434452522c0d72375bcb86
FailedToast <BeenForsaken@proton.me>
2025-09-20 03:00:23 -1000

    added shinji from the hip anime evangelion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
-const mockUrls = ["https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUHbL8mj54aGZkPvTRwxmYKXj62niHlcsqSOCA","https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUYG78nvPdIVanQs6E41G5xuBF0H8ARjboDmcO", "https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUYphrWxPdIVanQs6E41G5xuBF0H8ARjboDmcO"]
+const mockUrls = ["https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUHbL8mj54aGZkPvTRwxmYKXj62niHlcsqSOCA",
+	"https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUYG78nvPdIVanQs6E41G5xuBF0H8ARjboDmcO", 
+	"https://g6ls09toj2.ufs.sh/f/MnwCLV6PyBxUYphrWxPdIVanQs6E41G5xuBF0H8ARjboDmcO",
+	"https://k6fbvcjxww.ufs.sh/f/9pjQNL6bGuVg0SCiYd5RgDlmq4ofp6eTOkGPKZtBW12zQrcJ"]
 const mockImages = mockUrls.map((url, index) => ({
 	id: index + 1,
 	url,
