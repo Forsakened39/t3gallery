9443f144aa1653bfb30c9809b7cf2b0fc8225115
Forsaken39 <beenforsaken.proton.me>
2025-09-22 11:08:43 -1000

    added 2nd page, changed colors

--- src/app/layer02/page.tsx
+++ src/app/layer02/page.tsx
@@ -1,6 +1,7 @@
 export default function Layer2Page() {
-  return (
-    <main className="">
+  return ( //Layer 
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fafbf6] via-[#565a75] to-[#0f0f1b] text-white">
+
         (Second page yaaaas)
     </main>
   );
