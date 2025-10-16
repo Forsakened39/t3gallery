d0e1c57a2a1aa76d9d380a6e9af5b097e35adb9c
Forsaken39 <beenforsaken.proton.me>
2025-10-16 09:47:02 -1000

    added database to project

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,13 +1,15 @@
+/* safety line
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env";
 import * as schema from "./schema";
-
+//this is way your app will route to dataset
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
+/* safety line
 const globalForDb = globalThis as unknown as {
 	conn: postgres.Sql | undefined;
 };
@@ -16,3 +18,11 @@ const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
+safety line*/ 
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from "@vercel/postgres";
+import * as schema from "./schema";
+
+
+// Use this object to send drizzle queries to your DB
+export const db = drizzle(sql, {schema});
